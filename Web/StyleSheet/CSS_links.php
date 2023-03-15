02d53f304f0451b8baf50bf8e04c6c1361cb2306
Vinnie <lcvc@gmail.com>
2023-03-15 12:10:23 +0800

    add layout testing

---
+++ Web/StyleSheet/CSS_links.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+
+        <link href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/TARUMT_Event_Ticketing/Web/Library/MDB_BS5/css/mdb.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />-->
+    </head>
+    <body>
+    </body>
+</html>
