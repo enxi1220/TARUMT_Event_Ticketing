6c597ba569350d6ae038c1877ce2369c627da6ce
enxi1220 <enxilim024@gmail.com>
2023-04-18 23:21:42 +0800

    custom error pages, event decimal places

---
+++ Web/View/CustomError/BadRequest.php
@@ -0,0 +1,11 @@
+<?php
+require '../Layout.php';
+?>
+
+<div class="container text-center mt-5 pt-5">
+    <h1><i class="fa-regular fa-face-frown fa-5x my-5"></i></h1>
+    <h1 class="display-2 fw-bolder">400 </h1>
+    <h1 class="fs-1 mb-5">Bad Request</h1>
+    <p class="fs-5">The server could not understand the request due to invalid syntax or missing information.</p>
+    <p class="fs-5">You may click the menu above "Event" to browse events.</p>
+</div>
---
+++ Web/View/CustomError/Unauthorized.php
@@ -0,0 +1,11 @@
+<?php
+require '../Layout.php';
+?>
+
+<div class="container text-center mt-5 pt-5">
+    <h1><i class="fa-regular fa-face-frown fa-5x my-5"></i></h1>
+    <h1 class="display-2 fw-bolder">401 </h1>
+    <h1 class="fs-1 mb-5">Unauthorized</h1>
+    <p class="fs-5">You do not have permission to access this resource. Please log in or contact the site administrator.</p>
+    <p class="fs-5">You may click the menu above "Event" to browse events.</p>
+</div>
